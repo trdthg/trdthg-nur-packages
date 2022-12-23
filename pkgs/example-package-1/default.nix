80411b64eb0c5d2da8139f7a4ebd76fceb392764
trdthg <trdthg47@gmail.com>
2022-12-23 22:45:09 +0800

    remove example package
    Signed-off-by: trdthg <trdthg47@gmail.com>

--- pkgs/example-package-1/default.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv }:
-
-stdenv.mkDerivation rec {
-  name = "example-package-1-${version}";
-  version = "1.0";
-  src = ./.;
-  buildPhase = "echo echo Hello World > example";
-  installPhase = "install -Dm755 example $out";
-}
